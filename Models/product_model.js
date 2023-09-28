54f19905e9bde7698284e757a0df5f29c9d267aa
chetan791 <chetanchauhan795@gmail.com>
2023-09-29 03:04:05 +0530

    finished all

--- Models/product_model.js
+++ Models/product_model.js
@@ -4,7 +4,7 @@ const product_schema = new mongoose.Schema(
   {
     image: { type: String, require: true },
     brand: { type: String, require: true },
-    title: { type: Number, require: true },
+    title: { type: String, require: true },
     details: { type: String, require: true },
     price: { type: Number, require: true },
     category: { type: String, require: true },
