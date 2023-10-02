7fb1ccdd0ea929c2059dc60463ed254b12c5af76
chetan791 <chetanchauhan795@gmail.com>
2023-10-03 03:21:14 +0530

    appointment

--- Routes/service_route.js
+++ Routes/service_route.js
@@ -1,47 +1,55 @@
 const express = require("express");
 const auth = require("../middleware/auth.middlware");
-const nodemailer = require('nodemailer');
-const randomstring = require('randomstring');
+const nodemailer = require("nodemailer");
+const randomstring = require("randomstring");
 const Service_model = require("../Models/service_model");
 const { Otp_model } = require("../Models/Otp_model");
 const serviceRouter = express.Router();
-require('dotenv').config()
+require("dotenv").config();
+
+serviceRouter.get("/getappointment", async (req, res) => {
+  try {
+    const data = await Service_model.find();
+    res.send({ data: data });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 //////////EMAIL TRASNPORTER////////////
 
 let transporter = nodemailer.createTransport({
-  host:"smtp.gmail.com",
-  port:465,
-  secure:true,
-  auth : {
-      user : process.env.EMAIL_USERNAME,
-      pass : 'wjhb twwy zkbe peno'
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: "wjhb twwy zkbe peno",
   },
-})
-
-serviceRouter.post('/request_otp',(req, res) => {
-  // Generate a random OTP
-  const email = req.body.email
-  //console.log(email);
-  if(email){
-    sendOTPverification(email,res)
-  }
-  else{
-    res.status(404).send('Please provid the Email')
-  }
- 
 });
-const sendOTPverification = async(email,res)=>{
+
+serviceRouter.post("/request_otp", (req, res) => {
+  // Generate a random OTP
+  const email = req.body.email;
+  //console.log(email);
+  if (email) {
+    sendOTPverification(email, res);
+  } else {
+    res.status(404).send("Please provid the Email");
+  }
+});
+const sendOTPverification = async (email, res) => {
   console.log(email);
-  try{
-      const otp = `${Math.floor(1000+Math.random()*9000)}`
-      //localStorage.setItem('otp',otp)
-      const new_otp = await Otp_model.create({otp:otp})
-      new_otp.save()
-      const mailOption = {
-          from : process.env.EMAIL_USERNAME,
-          to : `${email}`,
-          subject : "Verify Your Email",
-          html :  `
+  try {
+    const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
+    //localStorage.setItem('otp',otp)
+    const new_otp = await Otp_model.create({ otp: otp });
+    new_otp.save();
+    const mailOption = {
+      from: process.env.EMAIL_USERNAME,
+      to: `${email}`,
+      subject: "Verify Your Email",
+      html: `
           <div style="background-color: #f2f2f2; padding: 20px;">
             <h2 style="color: #ff0000;">Verify Your Email</h2>
             <h1>Welcome to Paaltoo</h1>
@@ -51,40 +59,32 @@ const sendOTPverification = async(email,res)=>{
             </div>
           </div>
         `,
-
-
-      }
-      await transporter.sendMail(mailOption)
-      res.send({ msg:'OTP send'})
-  } 
-  catch(err){
-      res.send({err:err})
+    };
+    await transporter.sendMail(mailOption);
+    res.send({ msg: "OTP send" });
+  } catch (err) {
+    res.send({ err: err });
   }
-}
-serviceRouter.post('/verify_otp',async(req,res)=>{
-  const {otp} = req.body
+};
+serviceRouter.post("/verify_otp", async (req, res) => {
+  const { otp } = req.body;
   //console.log(otp);
-  try{
-    const stored_otp = await Otp_model.find({otp:otp})
+  try {
+    const stored_otp = await Otp_model.find({ otp: otp });
     //console.log(stored_otp);
-    if(stored_otp.length>0){
-     // console.log(stored_otp[0].otp==otp);
-      if(stored_otp[0].otp == otp){
-        await Otp_model.deleteMany({})
-         return   res.status(200).send('OTP verified SuccessFully')
+    if (stored_otp.length > 0) {
+      // console.log(stored_otp[0].otp==otp);
+      if (stored_otp[0].otp == otp) {
+        await Otp_model.deleteMany({});
+        return res.status(200).send("OTP verified SuccessFully");
+      } else {
+        return res.status(404).send("Invalid OTP");
       }
-      else{
-        return res.status(404).send('Invalid OTP')
-      }
+    } else {
+      res.status(404).send("Invalid OTP");
     }
-    else{
-      res.status(404).send('Invalid OTP')
-    }
-  }
-  catch(err){
-
-  }
-})
+  } catch (err) {}
+});
 ///////////////DO NOT TOUCH////////////
 serviceRouter.post("/register", auth, async (req, res) => {
   console.log(req.body);
@@ -96,7 +96,6 @@ serviceRouter.post("/register", auth, async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
-
 
 serviceRouter.get("/", async (req, res) => {
   try {
